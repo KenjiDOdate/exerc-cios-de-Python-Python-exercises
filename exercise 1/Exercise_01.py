8ec52d9da893ec735187272e78e7d6432c2be297
KenjiDOdate <kdodate@id.uff.br>
2024-01-08 19:33:31 -0300

    resolução simples
    uma resolução simplificada

---
+++ exercise 1/Exercise_01.py
@@ -0,0 +1,10 @@
+#exercicio 1 - escreva um algoritimo para ler um valor (do teclado) e escrever na tela o seu antecessor
+#exercise 1 - write an algorithm to read a value (from the keyboard) and print its predecessor on screen
+
+print("Por favor, insira um valor inteiro:")
+valor = int(input("Please, insert a integer value:"))
+
+resultado = valor - 1
+
+print(f"O antecessor de {valor} : {resultado}")
+print(f"The predecessor of {valor} : {resultado}")
--- exercise 1/exercise_1.py
+++
@@ -1,4 +0,0 @@
-#exercicio 1 - escreva um algoritimo para ler um valor (do teclado) e escrever na tela o seu antecessor
-#exercise 1 - write an algorithm to read a value (from the keyboard) and print its predecessor on screen
-
-print("Hello, World!")
