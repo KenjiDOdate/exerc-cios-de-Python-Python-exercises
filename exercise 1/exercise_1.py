86d0dfffc34001910aeaa5c65d79c8238d5699e7
KenjiDOdate <kdodate@id.uff.br>
2024-01-08 18:35:00 -0300

    correção de bug
    um bug com os comentários, utilizando o print como uma forma de testar se os códigos funcionariam normalmente

--- exercise 1/exercise_1.py
+++ exercise 1/exercise_1.py
@@ -1,2 +1,4 @@
-#exercício 1 - escreva um algoritimo para ler um valor (do teclado) e escrever na tela o seu antecessor
+#exercicio 1 - escreva um algoritimo para ler um valor (do teclado) e escrever na tela o seu antecessor
 #exercise 1 - write an algorithm to read a value (from the keyboard) and print its predecessor on screen
+
+print("Hello, World!")
