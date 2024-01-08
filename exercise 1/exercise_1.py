c6e672233b49dfc9210f79aad98c56b7df070f50
KenjiDOdate <kdodate@id.uff.br>
2024-01-07 22:42:09 -0300

    criação do exercício 1
    não possui a resolução ainda, só o corpo do programa pra começar, onde tem apenas 2 comentários falando o exercício, um em português e outro em inglês

---
+++ exercise 1/exercise_1.py
@@ -0,0 +1,2 @@
+#exercício 1 - escreva um algoritimo para ler um valor (do teclado) e escrever na tela o seu antecessor
+#exercise 1 - write an algorithm to read a value (from the keyboard) and print its predecessor on screen
