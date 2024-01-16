f15eca381713983c91c75091db6a91f539a118b7
KenjiDOdate <kdodate@id.uff.br>
2024-01-15 21:11:37 -0300

    exercício 2
    resolução do exercício 2

---
+++ exercise 2/exercise 2/exercise_2.py
@@ -0,0 +1,14 @@
+#escreva um algoritmo para ler as dimensoes de um retangulo (base e altura) e calcule a sua area
+#write an algorithm to read the dimensions of a rectangle (base and height) and calculate its area
+
+print("inform the base:")
+base = int(input("informe a base:"))
+
+print("\ninform the height:")
+height = int(input("informe a altura:"))
+
+area = base * height
+
+print(f"\nO retangulo com base {base} e altura {height} tem area igual {area}.")
+print(f"The retangulo with base {base} and height {height} have area equal {area}.\n")
+
