135f73fe189ff1d722fb963d0968af6070335eb2
KenjiDOdate <kdodate@id.uff.br>
2024-01-20 22:15:03 -0300

    exercício 3

--- Exercise 3/Exercise 3/Exercise_3.py
+++ Exercise 3/Exercise 3/Exercise_3.py
@@ -1,1 +1,2 @@
-
+# algoritmo que le idade de uma pessoa expressa em anos, meses e dias e escreve a idade em dias. Considerando o ano 365 dias e mes 30 dias
+# algorithm thar reads person's age expressed in years, months and days and writes the age in days. Considering the year 365 and month 30
